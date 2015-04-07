fd19af61b35ae6b7b10b1e1fde6bf8330e645b2b
Laura Cabrera <lzcabrera@gmail.com>
2015-04-06 21:13:45 -0700

    implement delete refactor

--- app/routes/friends/index.js
+++ app/routes/friends/index.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    delete: function(friend) {
-      var _this = this;
-
-      friend.destroyRecord().then(function() {
-        _this.transitionTo('friends.index');
-      });
-    }
+  model: function() {
+    return this.store.findAll('friend');
   }
 });
