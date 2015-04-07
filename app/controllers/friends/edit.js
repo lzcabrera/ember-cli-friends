690cfc49be9bb1d7e71fbc3caa7e56b6538f248c
Laura Cabrera <lzcabrera@gmail.com>
2015-04-06 20:34:26 -0700

    friends base - refactor of common code

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -1,30 +1,7 @@
-import Ember from 'ember';
+import FriendsBaseController from './base';
 
-export default Ember.Controller.extend({
-  isValid: Ember.computed(
-    'model.email',
-    'model.firstName',
-    'model.lastName',
-    'model.twitter',
-    function() {
-      return !Ember.isEmpty(this.get('model.email')) &&
-      !Ember.isEmpty('model.firstName') &&
-      !Ember.isEmpty('model.lastName') &&
-      !Ember.isEmpty('model.twitter');
-    }
-  ),
+export default FriendsBaseController.extend({
   actions: {
-  save: function() {
-    if(this.get('isValid')) {
-      var _this = this;
-      this.get('model').save().then(function(friend) {
-        _this.transitionToRoute('friends.show', friend);
-      });
-    } else {
-      this.set('errorMessage', 'You have to fill all the fields');
-    }
-    return false;
-  },
   cancel: function() {
     this.transitionToRoute('friends.show', this.get('model'));
     return false;
