e9f2acaf0e1323b8fbbf51f3e6817ab9e11144c9
Laura Cabrera <lzcabrera@gmail.com>
2015-04-23 23:37:53 -0700

    add ic-ajax as a bower component

--- Brocfile.js
+++ Brocfile.js
@@ -35,4 +35,17 @@ app.import('bower_components/picnic/releases/plugins.min.css');
 
 app.import('bower_components/moment/moment.js');
 
+app.import('bower_components/ic-ajax/dist/named-amd/main.js', {
+exports: {
+  'ic-ajax': [
+    'default',
+    'defineFixture',
+    'lookupFixture',
+    'raw',
+    'request',
+  ]
+}
+
+});
+
 module.exports = app.toTree();
