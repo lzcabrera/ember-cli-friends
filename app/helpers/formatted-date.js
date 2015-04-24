c03e0aaf8cc8b9c07d36fbc1344fb8a546017907
Laura Cabrera <lzcabrera@gmail.com>
2015-04-23 21:45:41 -0700

    add formattedDate helper using formatDate util

---
+++ app/helpers/formatted-date.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+import { formatDate } from '../utils/date-helpers';
+
+export function formattedDate(/*params, hash*/) {
+}
+
+export default Ember.Handlebars.makeBoundHelper(function(date, format){
+  return formatDate(date, format);
+});
