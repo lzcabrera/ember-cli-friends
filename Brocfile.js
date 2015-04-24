4594cfb07878842de6f3c52aec1229e603fae8c1
Laura Cabrera <lzcabrera@gmail.com>
2015-04-23 21:39:15 -0700

    add formatDate helper

--- Brocfile.js
+++ Brocfile.js
@@ -33,4 +33,6 @@ app.import('vendor/fontello/font/fontello.woff', {
 
 app.import('bower_components/picnic/releases/plugins.min.css');
 
+app.import('bower_components/moment/moment.js');
+
 module.exports = app.toTree();
